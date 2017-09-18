d07583ba71d7b1931ac63d660186535f12e4022b
SHawnHardy <shawn_work@outlook.com>
2017-09-19 00:21:22 +0800

    20170919

---
+++ 51nod/51nod1000/main.cpp
@@ -0,0 +1,28 @@
+#include "cstdio"
+#include "cstdlib"
+#include "cstring"
+#include "cmath"
+#include "ctime"
+#include "iostream"
+#include "algorithm"
+#include "string"
+#include "vector"
+#include "queue"
+#include "stack"
+#include "set"
+#include "map"
+
+using namespace std;
+
+typedef long long int ll;
+typedef pair<int,int> pii;
+typedef pair<int,string> pis;
+
+const int maxn=10010;
+
+int main() {
+    int a,b;
+    scanf("%d%d",&a,&b);
+    printf("%d\n",a+b);
+    return 0;
+}
