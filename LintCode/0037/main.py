feb9dcafbb23a4698e53cf19c627aaa265e5681c
SHawnHardy <shawn_work@outlook.com>
2018-07-12 10:50:14 +0800

    [Add]LintCode_0037_python3

---
+++ LintCode/0037/main.py
@@ -0,0 +1,9 @@
+class Solution:
+    """
+    @param number: A 3-digit number.
+    @return: Reversed number.
+    """
+
+    def reverseInteger(self, number):
+        # write your code here
+        return (number % 10) * 100 + (number // 10 % 10) * 10 + number // 100
