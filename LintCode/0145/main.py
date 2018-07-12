eb32cbb5b6d7bd7fcf970e47079d632e9f027393
SHawnHardy <shawn_work@outlook.com>
2018-07-12 11:06:42 +0800

    [Add]LintCode_0145_python3

---
+++ LintCode/0145/main.py
@@ -0,0 +1,10 @@
+class Solution:
+    """
+    @param character: a character
+    @return: a character
+    """
+
+    def lowercaseToUppercase(self, character):
+        # write your code here
+        return character.upper()
+
